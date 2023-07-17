743719bf5d612916038483fa55278bd4a51f0c06
Alisa Skrypnyk <topt18@gmail.com>
2023-07-17 15:47:39 +0300

    add task solution

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+var sliderIndex = 1;
+const slides = ["slide1.webp", "slide2.webp", "slide3.webp", "slide4.webp", "slide5.webp"];
+
+window.onload = function() {
+
+  document.getElementById('slideLeft').addEventListener('click', leftSlide);
+  document.getElementById('slideRight').addEventListener('click', rightSlide);
+  // document.getElementById('featured').src = "images/slide1.png";
+  
+};
+
+
+
 const dots = document.querySelectorAll(".dot-container button");
 const images = document.querySelectorAll(".image-container img");
 
@@ -17,66 +30,45 @@ function prev(){
   document.getElementById("content" + (i+1)).classList.add("active");
   indicator(i+1);
 }
-function indicator(num){
-  dots.forEach(function(dot){
-      dot.style.backgroundColor = "transparent";
-  });
-  document.querySelector(".dot-container button:nth-child(" + num + ")").style.backgroundColor = "#076bb8";
+// function indicator(num){
+//   dots.forEach(function(dot){
+//       dot.style.backgroundColor = "transparent";
+//   });
+//   document.querySelector(".dot-container button:nth-child(" + num + ")").style.backgroundColor = "#076bb8";
+// }
+
+// function dot(index){
+//   images.forEach(function(image){
+//       image.classList.remove("active");
+//   });
+//   document.getElementById("content" + index).classList.add("active");
+//   i = index - 1;
+//   indicator(index);
+// }
+
+function getImage(){
+
+  if (sliderIndex > slides.length){
+      sliderIndex = 1;
+
+  }
+   if (sliderIndex < 1){
+      sliderIndex = slides.length;
+   }
+
+  return slides[sliderIndex - 1];
 }
 
-function dot(index){
-  images.forEach(function(image){
-      image.classList.remove("active");
-  });
-  document.getElementById("content" + index).classList.add("active");
-  i = index - 1;
-  indicator(index);
+function leftSlide(){
+  sliderIndex -= 1;
+  var imageSrc = "images/" + getImage();
+  document.getElementById('featured').src = imageSrc;
 }
 
-// const mainImage = document.getElementById("main-image");
-// const images = document.querySelectorAll(".product__image");
-
-// images.forEach((image) => {
-//     image.addEventListener("click", (event) => {
-//         mainImage.src = event.target.src;
-
-//         document
-//             .querySelector(".product__image--active")
-//             .classList.remove("product__image--active");
-
-//         event.target.classList.add("product__image--active");
-//     });
-// });
+function rightSlide(){
+  sliderIndex += 1;
+  var imageSrc = "images/" + getImage();
+  document.getElementById('featured').src = imageSrc;
+}
 
 
-
-let thumbnails = document.getElementsByClassName('thumbnail')
-
-		let activeImages = document.getElementsByClassName('active')
-
-		for (let i = 0; i < thumbnails.length; i++){
-
-			thumbnails[i].addEventListener('mouseover', function(){
-				console.log(activeImages)
-				
-				if (activeImages.length > 0){
-					activeImages[0].classList.remove('active')
-				}
-				
-
-				this.classList.add('active')
-				document.getElementById('featured').src = this.src
-			})
-		}
-
-
-		let buttonRight = document.getElementById('slideRight');
-		let buttonLeft = document.getElementById('slideLeft');
-
-		buttonLeft.addEventListener('click', function(){
-			document.getElementById('featured').scrollLeft -= 180
-		})
-
-		buttonRight.addEventListener('click', function(){
-			document.getElementById('featured').scrollLeft += 180
-		})
